f51b51071b67b326bcc7f252b4aa46759a366e89
Burcu Geneci <burcu.geneci@gmail.com>
2022-06-10 16:02:54 -0700

    setup instructions - localization and router
    setup instructions for App Router and Easy Localization added.

--- lib/route/app_router.dart
+++ lib/route/app_router.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:mondayhero_starter_project/route/app_routes.dart';
 
-/// Set values as below where you configured MaterialApp.
-/// ```dart
-/// MaterialApp(
-///   // ...
-///   initialRoute: AppRoutes.init,
-///   onGenerateRoute: (settings) => AppRouter.generateRoute(settings),
-///   localizationsDelegates: context.localizationDelegates,
-///   locale: context.locale,
-///   supportedLocales: context.supportedLocales,
-///   // ...
-/// )
+/// SETUP APP ROUTER - Follow these steps 👇
+/// 1 - Go to your App definition
+/// 1.1 Set initialRoute and onGenerateRoute as follows, and
+/// 1.2 Delete home value if your app has one.
+/// ```Sample dart code:
+// MaterialApp(
+//   // ...
+//   initialRoute: AppRoutes.init,
+//   onGenerateRoute: (settings) => AppRouter.generateRoute(settings),
+//   // ...
+// )
+/// ```
+
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
