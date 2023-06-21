9b9ed66347dfb9819559b970b09840a36b0679cd
Neslihan Çekiç <neslihan@mondayhero.io>
2023-06-21 14:11:59 +0300

    intl support added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:mondayhero_starter_project/core/localization/mh_localization_asset_loader.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mondayhero_starter_project/route/app_router.dart';
 import 'package:mondayhero_starter_project/route/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [
-        Locale('en', 'US'),
-      ],
-      path: 'assets/translations',
-      assetLoader: const MHLocalizationAssetLoader(),
-      fallbackLocale: const Locale('en', 'US'),
-      child: const MyApp(),
-    ),
-  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -26,12 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      onGenerateTitle: (context) => 'appTitle'.tr(),
+      onGenerateTitle: (context) => "Monday Hero Starter Project",
       initialRoute: AppRoutes.init,
       onGenerateRoute: (settings) => AppRouter.generateRoute(settings),
-      localizationsDelegates: context.localizationDelegates,
-      locale: context.locale,
-      supportedLocales: context.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
